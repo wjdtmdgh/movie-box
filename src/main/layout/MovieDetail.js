8ad05f24319bb0d45049b57a67ddfd491b5cd630
wjdtmdgh <72695452+wjdtmdgh@users.noreply.github.com>
2023-02-07 23:55:12 +0900

    movieDetail 페이지 구현

--- src/main/layout/MovieDetail.js
+++ src/main/layout/MovieDetail.js
@@ -5,16 +5,21 @@ import {Card} from "antd";
 
 function  MovieDetail(){
     const {movieId} = useParams();
-    const [image, setImage] = useState([]);
+    const [movieDetail,setMovieDetail] = useState();
     useEffect(() => {
-        webClient.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=17782ac9805895b80c9219fa3809a24c&language=en-US`)
+        webClient.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=17782ac9805895b80c9219fa3809a24c&language=ko-kr`)
             .then((res) => {
-                setImage(res.data.results);
+                setMovieDetail(res.data);
             })
     }, [])
+    useEffect(()=>{
+        console.log(movieDetail);
+    },[movieDetail])
     return(
         <div>
-                  <img alt="영화 포스터" src={`https://image.tmdb.org/t/p/w500/${movieId}/anHwj9IupRoRZZ98WTBvHpTiE6A.jpg`} />
+            {movieDetail &&
+                <img alt="영화 포스터" src={`https://image.tmdb.org/t/p/w500/${movieDetail.poster_path}`} />
+            }
             <p>{movieId}</p>
         </div>
     );
